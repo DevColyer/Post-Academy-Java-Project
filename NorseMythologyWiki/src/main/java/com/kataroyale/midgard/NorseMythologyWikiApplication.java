cebd7540f505f67366c2bdca13ec60fc1ba54298
DevColyer <dcolyer@spartaglobal.com>
2024-08-05 15:00:30 +0100

    Project initialisation

---
+++ NorseMythologyWiki/src/main/java/com/kataroyale/midgard/NorseMythologyWikiApplication.java
@@ -0,0 +1,13 @@
+package com.kataroyale.midgard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorseMythologyWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NorseMythologyWikiApplication.class, args);
+    }
+
+}
---
+++ NorseMythologyWiki/src/test/java/com/kataroyale/midgard/NorseMythologyWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kataroyale.midgard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorseMythologyWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
